f7f1a668ca2c6a5a44eed33964361273fbcaf6d0
Irene Dovichi <126991310+irenedovichi@users.noreply.github.com>
2023-12-10 19:40:47 +0100

    added functional.py

---
+++ functional.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+
+class Function:
+
+    def __call__(self, x):
+        raise NotImplementedError("Function.__call__() not implemented.")
+
+    def df(self, x):
+        raise NotImplementedError("Function.df() not implemented.")
+
+
+class Sigmoid(Function):
+
+    def df(self, x):
+        return self.__call__(x) * (1 - self.__call__(x))
+
+    def __call__(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def __str__(self) -> str:
+        return "Sigmoid"
+
+
+class ReLU(Function):
+
+    def df(self, x):
+        return np.where(x <= 0, 0, 1)
+
+    def __call__(self, x):
+        return np.maximum(0, x)
+
+    def __str__(self) -> str:
+        return "ReLU"
+
+
+class Tanh(Function):
+
+    def df(self, x):
+        return 1 - np.tanh(x) ** 2
+
+    def __call__(self, x):
+        return np.tanh(x)
+
+    def __str__(self) -> str:
+        return "Tanh"
+
+
+class Softmax(Function):
+
+    def df(self, x):
+        return self.__call__(x) * (1 - self.__call__(x))
+
+    def __call__(self, x):
+        return np.exp(x) / np.sum(np.exp(x), axis=1, keepdims=True)
+
+    def __str__(self) -> str:
+        return "Softmax"
--- layer.py
+++ layer.py
@@ -3,36 +3,36 @@ import numpy as np
 
 class HiddenLayer:
     """Class that represents a hidden layer of the network."""
-    def __init__(self, num_inputs, num_units, activation):
-        """Initialize the hidden layer with num_inputs inputs,
-        num_units units, and the specified activation function.
+    def __init__(self, input_size, units_size, activation):
+        """Initialize the hidden layer with input_size inputs,
+        units_size units, and the specified activation function.
         """
-        self.num_inputs = num_inputs
-        self.num_units = num_units
+        self.input_size = input_size
+        self.units_size = units_size
 
         self.activation = activation
         # Initialize weights and biases
         # TODO: initial initialization of weights and biases is currently random
-        self.setup(num_inputs, num_units)
+        self.setup(input_size, units_size)
 
-    def setup(self, num_inputs, num_units):
+    def setup(self, input_size, units_size):
         """Setups the weights and biases of the layer."""
 
         # Create a weight matrix of appropriate shape; the order is reversed
         # because we want the shape to be (n_inputs, n_neurons)
-        self.W = np.random.uniform(low=-0.5, high=0.5, size=(num_inputs, num_units))
+        self.W = np.random.uniform(low=-0.5, high=0.5, size=(input_size, units_size))
 
         # Create a bias vector of appropriate shape; the first parameter of zeros()
         # is the shape of the array, in this case it is a 1D array with n_neurons elements
-        self.b = np.random.uniform(low=-0.5, high=0.5, size=(1, num_units))
+        self.b = np.random.uniform(low=-0.5, high=0.5, size=(1, units_size))
 
     def forward(self, inputs: np.ndarray):
         """Forwards the output of the layer units."""
         self.net = inputs.dot(self.W) + self.b
-        self.o = self.activation(self.net)
-        return self.o
+        self.out = self.activation(self.net)
+        return self.out
     
     def __str__(self) -> str:
         """Returns a string description of the layer."""
-        return (f"HiddenLayer(num_units={self.num_units}, num_inputs={self.num_inputs},"
+        return (f"HiddenLayer(units_size={self.units_size}, input_size={self.input_size},"
                 f"activation={self.activation.__name__})\nW={self.W}\nb={self.b})")
