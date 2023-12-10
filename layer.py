6d29595202033cf245954638a86fac81d2c6cb17
lavo <45769654+chucki1@users.noreply.github.com>
2023-12-10 15:20:58 +0100

    changed hidden layer, added 'net' and 'o' to follow course func names

--- layer.py
+++ layer.py
@@ -1,12 +1,20 @@
 import numpy as np
 
 class HiddenLayer:
-    def __init__(self, num_inputs, num_units, activation):
-        self.num_inputs = num_inputs
-        self.num_units = num_units
-        self.activation = activation
+    def __init__(self, inputs, num_units, activation):
 
-        self.setup(num_inputs, num_units)
+        self.inputs = inputs
+        self.num_inputs = inputs.shape
+
+        self.num_units = num_units
+
+        self.activation = activation
+        # Initialize weights and biases
+        # TODO: initial initialization of weights and biases is currently random
+        self.setup(inputs, num_units)
+        
+        self.net = np.dot(inputs, self.W) + self.b
+        self.o = self.activation(self.net)
 
     def setup(self, num_inputs, num_units):
         # Create a weight matrix of appropriate shape
@@ -14,12 +22,8 @@ class HiddenLayer:
         # Create a bias vector of appropriate shape
         self.b =  np.random.uniform(low=-0.5, high=0.5, size=(1, num_units)) # the first parameter of zeros() is the shape of the array, in this case it is a 1D array with n_neurons elements
 
-    def forward(self, inputs):
-        # Perform matrix multiplication of input and weights 1xn * nxm = 1xm
-        a = np.dot(inputs, self.W) + self.b
-        # Apply activation function
-        self.output = self.activation(a)
-        return self.output
+    def forward(self):
+        return self.o
     
     def __str__(self) -> str:
         return f"HiddenLayer(num_units={self.num_units}, num_inputs={self.num_inputs}, activation={self.activation.__name__})\nW={self.W}\nb={self.b})"
