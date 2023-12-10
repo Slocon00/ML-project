51a7b035757de92a2fa09b764665e240ba885739
lavo <45769654+chucki1@users.noreply.github.com>
2023-12-10 16:20:25 +0100

    fixed problems, last notebook cells for test

--- layer.py
+++ layer.py
@@ -1,20 +1,16 @@
 import numpy as np
 
 class HiddenLayer:
-    def __init__(self, inputs, num_units, activation):
+    def __init__(self, num_inputs, num_units, activation):
 
-        self.inputs = inputs
-        self.num_inputs = inputs.shape
-
+        self.num_inputs = num_inputs
         self.num_units = num_units
 
         self.activation = activation
         # Initialize weights and biases
         # TODO: initial initialization of weights and biases is currently random
-        self.setup(inputs, num_units)
+        self.setup(num_inputs, num_units)
         
-        self.net = np.dot(inputs, self.W) + self.b
-        self.o = self.activation(self.net)
 
     def setup(self, num_inputs, num_units):
         # Create a weight matrix of appropriate shape
@@ -22,7 +18,9 @@ class HiddenLayer:
         # Create a bias vector of appropriate shape
         self.b =  np.random.uniform(low=-0.5, high=0.5, size=(1, num_units)) # the first parameter of zeros() is the shape of the array, in this case it is a 1D array with n_neurons elements
 
-    def forward(self):
+    def forward(self, inputs):
+        self.net = np.dot(inputs, self.W) + self.b
+        self.o = self.activation(self.net)
         return self.o
     
     def __str__(self) -> str:
