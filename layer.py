Fix backprop bias

layer.py
@@ -78,13 +78,12 @@ class HiddenLayer:
         delta_prop = self.W.dot(delta)
         if self.regularizer is not None:
             self.W -= eta * delta_grad - alpha * self.delta_old + self.regularizer.derivative(W)
-            self.delta_old = - (eta * delta_grad) #TODO: sign is correct?
+            self.delta_old = - (eta * delta_grad)
         else:
             self.W -= eta * delta_grad - alpha * self.delta_old
-            self.delta_old = - (eta * delta_grad) #TODO: sign is correct?
+            self.delta_old = - (eta * delta_grad)
 
-        # TODO: regularization on the bias?
-        self.b -= eta * np.sum(delta, axis=0, keepdims=True)
+        self.b -= eta * delta
         return delta_prop
 
     def __str__(self) -> str: